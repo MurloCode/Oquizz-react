c5dcf3853b8d8a61886ffcf89881bdc9e757e3ab
Corentin Marillier <marillier.io@gmail.com>
2021-07-08 23:28:52 +0200

    installation de react-bootstrap 4.6.0 + modification component Oquizz(homepage)

---
+++ src/components/Oquizz/index.js
@@ -0,0 +1,19 @@
+// == Import npm
+import React from 'react';
+
+// == Import css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+// == Import
+//import Header from 'src/containers/Header';
+
+// == Composant
+const Oquizz = () => (
+  <div className="title">
+    <h1>Homepage disconnected</h1>
+  </div>
+);
+
+// == Export
+export default Oquizz;
